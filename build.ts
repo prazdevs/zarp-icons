dc51270b166d3385bbf2678323acb804c745e017
prazdevs <sachabouillez@gmail.com>
2022-08-13 00:52:53 +0200

    feat: setup build steps

--- build.ts
+++ build.ts
@@ -1,18 +1,73 @@
-import { readdirSync, writeFileSync } from "fs";
-import { default as theme } from './theme.json'
+import { remove, mkdirs } from 'fs-extra'
+import { readFile, writeFile, readdir } from 'fs/promises'
+import { optimize, OptimizedSvg } from 'svgo'
 
-const iconFiles = readdirSync("./icons");
-const iconDefinitions = iconFiles.reduce(
-  (acc, cur) => ({
-    ...acc,
-    [cur.slice(0, -4)]: `./icons/${cur}`,
-  }),
-  {}
-);
+const dist = `${process.cwd()}/dist`
 
-const r = {
-  ...theme,
-  iconDefinitions
+async function optimizeSvg(path: string) {
+  const file = await readFile(`./icon_sources/${path}`)
+  const optimized = optimize(file, {
+    js2svg: { pretty: true },
+    plugins: [
+      'removeDoctype',
+      'removeXMLProcInst',
+      'removeComments',
+      'removeMetadata',
+      'removeEditorsNSData',
+      'convertStyleToAttrs',
+      'cleanupAttrs',
+      'mergeStyles',
+      'inlineStyles',
+      'minifyStyles',
+      'cleanupIDs',
+      'removeRasterImages',
+      'cleanupNumericValues',
+      'convertColors',
+      'removeUnknownsAndDefaults',
+      'removeNonInheritableGroupAttrs',
+      'removeUselessStrokeAndFill',
+      'removeViewBox',
+      'cleanupEnableBackground',
+      'removeHiddenElems',
+      'removeEmptyText',
+      'convertShapeToPath',
+      'moveElemsAttrsToGroup',
+      'moveGroupAttrsToElems',
+      'collapseGroups',
+      'convertPathData',
+      'convertEllipseToCircle',
+      'convertTransform',
+      'removeEmptyAttrs',
+      'removeEmptyContainers',
+      'mergePaths',
+      'removeUnusedNS',
+      'sortAttrs',
+      'sortDefsChildren',
+      'removeTitle',
+      'removeDesc',
+    ],
+  }) as OptimizedSvg
+  await writeFile(`${dist}/icons/${path}`, optimized.data)
 }
 
-writeFileSync('theme.json', JSON.stringify(r, null, 2))
+//* optimize SVGs
+const sourceIcons = await readdir('./icon_sources')
+await remove(dist)
+await mkdirs(`${dist}/icons`)
+await Promise.all(sourceIcons.map(optimizeSvg))
+
+// const iconFiles = readdirSync('./icons')
+// const iconDefinitions = iconFiles.reduce(
+//   (acc, cur) => ({
+//     ...acc,
+//     [cur.slice(0, -4)]: `./icons/${cur}`,
+//   }),
+//   {},
+// )
+
+// const r = {
+//   ...theme,
+//   iconDefinitions,
+// }
+
+// writeFileSync('theme.json', JSON.stringify(r, null, 2))
