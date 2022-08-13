98b683763beab5e61fd3c55aba83ecd091e0035b
prazdevs <sachabouillez@gmail.com>
2022-08-14 00:23:53 +0200

    feat: add icons and todo

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,8 +1,6 @@
 export const extensions: IconMap = {
-  go: ['go'],
   js: ['esx', 'cjs'],
   mapjs: ['js.map', 'cjs.map', 'mjs.map'],
   dts: ['d.ts', 'd.cts', 'd.mts'],
-  rust: ['rs'],
 }
 
--- src/languages.ts
+++ src/languages.ts
@@ -1,6 +1,11 @@
 export const languages: IconMap = {
+  git: ['git', 'git-commit', 'git-rebase', 'ignore'],
+  go: ['go'],
   js: ['javascript'],
+  json: ['json', 'jsonc', 'json5'],
   jsx: ['javascriptreact'],
+  rust: ['rust'],
+  svelte: ['svelte'],
   ts: ['typescript'],
-  tsx: ['typescriptreact']
+  tsx: ['typescriptreact'],
 }
